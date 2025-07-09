9494bfbb55a007f826113f87a93e8122310fa551
smritee04 <smritigiri92@gmail.com>
2025-07-09 15:38:21 +0545

    Initial commit

---
+++ employee/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+    first_name=models.CharField(max_length=100)
+    middle_name=models.CharField(max_length=100,blank=True)
+    last_name=models.CharField(max_length=100)
+    email=models.EmailField(max_length=100,unique=True)
+    phone_number=models.CharField(max_length=12,blank=True)
+    image=models.ImageField(upload_to='images')
+
