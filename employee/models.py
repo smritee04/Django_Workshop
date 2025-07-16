cbf9dfeca93e667f4ee9a7cdfc378a95d5d060d9
smritee04 <smritigiri92@gmail.com>
2025-07-16 20:09:51 +0545

    "Add __str__ method to Employee model to show names in Django admin"

--- employee/models.py
+++ employee/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
-# Create your models here.
-class Employee(models.Model):
-    first_name=models.CharField(max_length=100)
-    middle_name=models.CharField(max_length=100,blank=True)
-    last_name=models.CharField(max_length=100)
-    email=models.EmailField(max_length=100,unique=True)
-    phone_number=models.CharField(max_length=12,blank=True)
-    
-    image=models.ImageField(upload_to='images')
 
-def _str_(self):
-    return self.first_name
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=12, blank=True)
+    image = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.first_name  
